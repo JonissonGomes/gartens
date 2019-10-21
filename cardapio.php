6a5b04641eaa3b5eadf2061e6c791c32da63871d
JonissonGomes <joni_gomes@hotmail.com>
2019-10-21 18:01:01 -0300

    Adicionando a página com os itens do cardápio

---
+++ cardapio.php
@@ -0,0 +1,466 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">	
+
+	<title> Cardápio - Garten's </title>
+
+	<!--Imports das fontes utilizadas-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <!--Import arquivos do materialize.css Offline-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+	
+    <!--Let browser know website is optimized for mobile-->
+     
+</head>
+<body>
+	<!-- Começo -->
+	<!-- Cabeçalho -->
+	<nav class=" amber darken-2" role="navigation">
+    <div class="nav-wrapper container">
+      <a id="logo-container" href="/index.php" class="brand-logo"><img src="img/logo.png" style="margin-top: 5%;"></a>
+      <ul class="right hide-on-med-and-down">
+      	<li><a href="/pratocustom.php">Pratos customizaveis</a></li>
+      </ul>
+		<!-- Mobile -->
+		<ul id="nav-mobile" class="sidenav">
+		<h4> 
+			<center><img src="/img/logo.png"></h4></center>
+		<hr>
+        <li><a href="#">Crie seu prato</a></li>
+      </ul><p>
+      <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+    </div>
+  </nav>
+
+	<!-- Lista de pratos -->
+
+	<div class="row container">
+		<div class="col s12"> <span class="flow-text"> <h3 class="orange-text">Cardápio - Padrão</h3> </span><hr> </div> 
+			<!-- Cardápio -->
+		<h5 class="row"> • Pratos executivos: </h5>
+			<!-- Prato 1 -->
+		<div class="col s3">
+			<div class="card">
+   				 <div class="card-image waves-effect waves-block waves-light">
+      				<img class="activator" src="img/pratos/frangoaparmesiana.jpg">
+   				 </div>
+    				<div class="card-content">
+    				  <span class="card-title activator orange-text text-darken-4">Frango à parmegiana<i class="material-icons right">more_vert</i></span>
+    				  <p>
+    				  	<strong class="flow-text"> R$ 19,90 </strong>
+    				  	<br>
+    				  	<a href="#">Fazer pedido</a></p>
+    				</div>
+   						 <div class="card-reveal">
+      						<span class="card-title orange-text text-darken-4">Descrição: <i class="material-icons right">close</i></span>
+      						<p>
+      							- Spaguett ao molho de tomate italiano artesanal natural <br> 
+      							- Purê de batatas, abobora ou batata frita <br>
+      							- Frango com queijo mussarela.
+      						</p>
+    					</div>
+ 					 </div>
+ 		</div>
+
+		<!-- Prato 2 -->
+	
+		<div class="col s3">
+			<div class="card">
+   				 <div class="card-image waves-effect waves-block waves-light">
+      				<img class="activator" src="img/pratos/bifeaparmesiana.jpeg">
+   				 </div>
+    				<div class="card-content">
+    				  <span class="card-title activator orange-text text-darken-4">Bife à parmesiana<i class="material-icons right">more_vert</i></span>
+    				  <p>
+    				  	<strong class="flow-text"> R$ 20,00 </strong>
+    				  	<br>
+    				  	<a href="#">Fazer pedido</a>
+    				  </p>
+    				</div>
+   						 <div class="card-reveal">
+      						<span class="card-title orange-text text-darken-4">Descrição: <i class="material-icons right">close</i></span>
+      						<p>
+      							- Spaguett ao molho de tomate italiano artesanal natural com purê de batata ou batata frita<br>
+      							- Bife com queijo mussarela<br>
+      						</p>
+    					</div>
+ 					 </div>
+ 				</div>
+
+ 				<!-- Prato 3 -->
+ 				<div class="col s3">
+			<div class="card">
+   				 <div class="card-image waves-effect waves-block waves-light">
+      				<img class="activator" src="img/pratos/feijoadaamodachefe.jpeg">
+   				 </div>
+    				<div class="card-content">
+    				  <span class="card-title activator orange-text text-darken-4">Feijoada à moda chefe <i class="material-icons right">more_vert</i></span>
+    				  <p>
+    				  	<strong class="flow-text"> R$ 20,00 </strong>
+    				  	<br>
+    				  	<a href="#">Fazer pedido</a>
+    				  </p>
+    				</div>
+   						 <div class="card-reveal">
+      						<span class="card-title orange-text text-darken-4">Descrição: <i class="material-icons right">close</i></span>
+      						<p>
+      							- Arroz branco<br>
+      							- Couve <br>
+      							- Vinagrete e Farofa ou batata frita.
+      						</p>
+    					</div>
+ 					 </div>
+ 				</div>
+
+ 				<!-- Prato 4 -->
+ 					<div class="col s3">
+			<div class="card">
+   				 <div class="card-image waves-effect waves-block waves-light">
+      				<img class="activator" src="img/pratos/tilapiaaopapelloti.jpg">
+   				 </div>
+    				<div class="card-content">
+    				  <span class="card-title activator orange-text text-darken-4">Tilápia ao papelloti<i class="material-icons right">more_vert</i></span>
+    				  <p>
+    				  	<strong class="flow-text"> R$ 25,90 </strong>
+    				  	<br>
+    				  	<a href="#">Fazer pedido</a>
+    				  </p>
+    				</div>
+   						 <div class="card-reveal">
+      						<span class="card-title orange-text text-darken-4">Descrição: <i class="material-icons right">close</i></span>
+      						<p>
+      							- Tilápia com legumes ao forno <br>
+      							- Arroz branco
+      						</p>
+    					</div>
+ 					 </div>
+ 				</div>
+	</div>
+			<!-- Cardápio de Massas -->
+			<div class="row container"><hr>
+			<h5 class="row"> • Massas: </h5>
+
+			<!-- Prato 1 -->
+		<div class="col s4">
+			<div class="card">
+   				 <div class="card-image waves-effect waves-block waves-light">
+      				<img class="activator" src="img/pratos/fetuccini.jpg">
+   				 </div>
+    				<div class="card-content">
+    				  <span class="card-title activator orange-text text-darken-4">Fetuccini alfredo<i class="material-icons right">more_vert</i></span>
+    				  <p>
+    				  	<strong class="flow-text"> R$ 29,90 </strong>
+    				  	<br>
+    				  	<a href="#">Fazer pedido</a></p>
+    				</div>
+   						 <div class="card-reveal">
+      						<span class="card-title orange-text text-darken-4">Descrição: <i class="material-icons right">close</i></span>
+      						<p>
+      							- Peito de frango salteado em azeite de ervas finas com brócolis <br> 
+      							- Pimenta <br>
+      							- Molho especial.
+      						</p>
+    					</div>
+ 					 </div>
+ 				</div>
+
+		<!-- Prato 2 -->
+	
+		<div class="col s4">
+			<div class="card">
+   				 <div class="card-image waves-effect waves-block waves-light">
+      				<img class="activator" src="img/pratos/espaguetecarbonara.jpg">
+   				 </div>
+    				<div class="card-content">
+    				  <span class="card-title activator orange-text text-darken-4">Spaguette carbonara<i class="material-icons right">more_vert</i></span>
+    				  <p>
+    				  	<strong class="flow-text"> R$ 29,90 </strong>
+    				  	<br>
+    				  	<a href="#">Fazer pedido</a>
+    				  </p>
+    				</div>
+   						 <div class="card-reveal">
+      						<span class="card-title orange-text text-darken-4">Descrição: <i class="material-icons right">close</i></span>
+      						<p>
+      							- Molho branco especial italiano(azeite, oregano, manjericão, bacon e alho)<br>
+      							- Pimenta<br>
+      							- Calabresa <br>
+      							- Queijo parmesão.
+      						</p>
+    					</div>
+ 					 </div>
+ 				</div>
+
+ 				<!-- Prato 3 -->
+ 				<div class="col s4">
+			<div class="card">
+   				 <div class="card-image waves-effect waves-block waves-light">
+      				<img class="activator" src="img/pratos/nhoque.jpg">
+   				 </div>
+    				<div class="card-content">
+    				  <span class="card-title activator orange-text text-darken-4">Nhoque garten's<i class="material-icons right">more_vert</i></span>
+    				  <p>
+    				  	<strong class="flow-text"> R$ 32,90 </strong>
+    				  	<br>
+    				  	<br>
+    				  	<a href="#">Fazer pedido</a>
+    				  </p>
+    				</div>
+   						 <div class="card-reveal">
+      						<span class="card-title orange-text text-darken-4">Descrição: <i class="material-icons right">close</i></span>
+      						<p>
+      							- Nhoque de batata doce<br>
+      							- Molho branco especial<br>
+      							- Queijo gongorzola <br>
+      							- Alho granulado <br>
+      							- Ervas finas.
+      						</p>
+    					</div>
+ 					 </div>
+ 				</div>
+			</div>
+
+			<!-- Cardápio de bebidas -->
+		<div class="row container">
+		<hr> 
+		<h5 class="row"> • Bebidas: </h5>
+			<!-- Prato 1 -->
+		<div class="col s4">
+			<div class="card">
+   				 <div class="card-image waves-effect waves-block waves-light">
+      				<img class="activator" src="img/bebidas/agua.jpg">
+   				 </div>
+    				<div class="card-content">
+    				  <span class="card-title activator orange-text text-darken-4">Água mineral<i class="material-icons right">more_vert</i></span>
+    				  <p>
+    				  	<strong class="flow-text"> R$ 1,49 </strong>
+    				  	<br>
+    				  	<a href="#">Fazer pedido</a></p>
+    				</div>
+   						 <div class="card-reveal">
+      						<span class="card-title orange-text text-darken-4">Descrição: <i class="material-icons right">close</i></span>
+      						<p>
+      							- Água mineral - Indaia <br>
+      							- 300ml<br> 
+       						</p>
+    					</div>
+ 					 </div>
+ 		</div>
+
+		<!-- Prato 2 -->
+	
+		<div class="col s4">
+			<div class="card">
+   				 <div class="card-image waves-effect waves-block waves-light">
+      				<img class="activator" src="img/bebidas/h2o.jpg">
+   				 </div>
+    				<div class="card-content">
+    				  <span class="card-title activator orange-text text-darken-4">H2O<i class="material-icons right">more_vert</i></span>
+    				  <p>
+    				  	<strong class="flow-text"> R$ 6,90 </strong>
+    				  	<br>
+    				  	<a href="#">Fazer pedido</a>
+    				  </p>
+    				</div>
+   						 <div class="card-reveal">
+      						<span class="card-title orange-text text-darken-4">Descrição: <i class="material-icons right">close</i></span>
+      						<p>
+      							- Limoneto<br>
+      							- Limão<br>
+      							
+      						</p>
+    					</div>
+ 					 </div>
+ 				</div>
+
+ 				<!-- Prato 3 -->
+ 				<div class="col s4">
+			<div class="card">
+   				 <div class="card-image waves-effect waves-block waves-light">
+      				<img class="activator" src="img/bebidas/sprite.jpg">
+   				 </div>
+    				<div class="card-content">
+    				  <span class="card-title activator orange-text text-darken-4">Sprite <i class="material-icons right">more_vert</i></span>
+    				  <p>
+    				  	<strong class="flow-text"> R$ 5,90 </strong>
+    				  	<br>
+    				  	<a href="#">Fazer pedido</a>
+    				  </p>
+    				</div>
+   						 <div class="card-reveal">
+      						<span class="card-title orange-text text-darken-4">Descrição: <i class="material-icons right">close</i></span>
+      						<p>
+      							- Lata<br>
+      							- 350ml<br>
+      						</p>
+    					</div>
+ 					 </div>
+ 				</div>
+		</div>
+			<!-- Continuação das bebidas -->
+	<div class="row container">
+			<!-- Prato 1 -->
+		<div class="col s4">
+			<div class="card">
+   				 <div class="card-image waves-effect waves-block waves-light">
+      				<img class="activator" style="margin-bottom: 12%; margin-top: 4%;" src="img/bebidas/cocacola.jpg">
+   				 </div>
+    				<div class="card-content">
+    				  <span class="card-title activator orange-text text-darken-4">Coca Cola<i class="material-icons right">more_vert</i></span>
+    				  <p>
+    				  	<strong class="flow-text"> R$ 5,90 </strong>
+    				  	<br>
+    				  	<a href="#">Fazer pedido</a></p>
+    				</div>
+   						 <div class="card-reveal">
+      						<span class="card-title orange-text text-darken-4">Descrição: <i class="material-icons right">close</i></span>
+      						<p>
+      							- Lata <br>
+      							- 350ml<br> 
+       						</p>
+    					</div>
+ 					 </div>
+ 		</div>
+
+		<!-- Prato 2 -->
+	
+		<div class="col s4">
+			<div class="card">
+   				 <div class="card-image waves-effect waves-block waves-light">
+      				<img class="activator" style=" margin-top: 9%; " src="img/bebidas/cocacolazero.jpg">
+   				 </div>
+    				<div class="card-content">
+    				  <span class="card-title activator orange-text text-darken-4">Coca Cola Zero<i class="material-icons right">more_vert</i></span>
+    				  <p>
+    				  	<strong class="flow-text"> R$ 6,90 </strong>
+    				  	<br>
+    				  	<br>
+    				  	<a href="#">Fazer pedido</a>
+    				  </p>
+    				</div>
+   						 <div class="card-reveal">
+      						<span class="card-title orange-text text-darken-4">Descrição: <i class="material-icons right">close</i></span>
+      						<p>
+      							- Lata<br>
+      							- 350ml<br>
+      							
+      						</p>
+    					</div>
+ 					 </div>
+ 				</div>
+
+ 				<!-- Prato 3 -->
+ 				<div class="col s4">
+			<div class="card">
+   				 <div class="card-image waves-effect waves-block waves-light">
+      				<img class="activator" style="margin-top: 10%; margin-bottom: 6%; " src="img/bebidas/antartica.jpg">
+   				 </div>
+    				<div class="card-content">
+    				  <span class="card-title activator orange-text text-darken-4">Guaraná antártica <i class="material-icons right">more_vert</i></span>
+    				  <p>
+    				  	<strong class="flow-text"> R$ 5,90 </strong>
+    				  	<br>
+    				  	<a href="#">Fazer pedido</a>
+    				  </p>
+    				</div>
+   						 <div class="card-reveal">
+      						<span class="card-title orange-text text-darken-4">Descrição: <i class="material-icons right">close</i></span>
+      						<p>
+      							- Lata<br>
+      							- 350ml<br>
+      						</p>
+    					</div>
+ 					 </div>
+ 				</div>
+ 			</div>
+
+ 				<!-- continuação bebidas 2 -->
+
+<div class="row container">
+		<hr> 
+	<h5 class="row"> • Bebidas alcoólicas: </h5>
+			<!-- Prato 1 -->
+		<div class="col s4">
+			<div class="card">
+   				 <div class="card-image waves-effect waves-block waves-light">
+      				<img class="activator" src="img/bebidas/bud.png">
+   				 </div>
+    				<div class="card-content">
+    				  <span class="card-title activator orange-text text-darken-4">Budweiser<i class="material-icons right">more_vert</i></span>
+    				  <p>
+    				  	<strong class="flow-text"> R$ 5,90 </strong>
+    				  	<br>
+    				  	<a href="#">Fazer pedido</a></p>
+    				</div>
+   						 <div class="card-reveal">
+      						<span class="card-title orange-text text-darken-4">Descrição: <i class="material-icons right">close</i></span>
+      						<p>
+      							- Lata <br>
+      							- 330ml<br> 
+       						</p>
+    					</div>
+ 					 </div>
+ 		</div>
+
+		<!-- Prato 2 -->
+	
+		<div class="col s4">
+			<div class="card">
+   				 <div class="card-image waves-effect waves-block waves-light">
+      				<img class="activator" src="img/bebidas/heineken.jpg">
+   				 </div>
+    				<div class="card-content">
+    				  <span class="card-title activator orange-text text-darken-4">Heineken<i class="material-icons right">more_vert</i></span>
+    				  <p>
+    				  	<strong class="flow-text"> R$ 6,90 </strong>
+    				  	<br>
+    				  	<a href="#">Fazer pedido</a>
+    				  </p>
+    				</div>
+   						 <div class="card-reveal">
+      						<span class="card-title orange-text text-darken-4">Descrição: <i class="material-icons right">close</i></span>
+      						<p>
+      							- Garrafa<br>
+      							- 330ml<br>
+      							
+      						</p>
+    					</div>
+ 					 </div>
+ 				</div>
+
+ 				<!-- Prato 3 -->
+ 				<div class="col s4">
+			<div class="card">
+   				 <div class="card-image waves-effect waves-block waves-light">
+      				<img class="activator" src="img/bebidas/stella.jpg">
+   				 </div>
+    				<div class="card-content">
+    				  <span class="card-title activator orange-text text-darken-4">Stella Artois<i class="material-icons right">more_vert</i></span>
+    				  <p>
+    				  	<strong class="flow-text"> R$ 5,90 </strong>
+    				  	<br>
+    				  	<a href="#">Fazer pedido</a>
+    				  </p>
+    				</div>
+   						 <div class="card-reveal">
+      						<span class="card-title orange-text text-darken-4">Descrição: <i class="material-icons right">close</i></span>
+      						<p>
+      							- Lata<br>
+      							- 330ml<br>
+      						</p>
+    					</div>
+ 					 </div>
+ 				</div>
+
+
+
+	<!-- Fim -->
+	<!-- Biblioteca de JavaScript -->
+       <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+      <script src="js/materialize.min.js"></script>
+       <script src="js/init.js"></script>
+
+</body>
+</html>
