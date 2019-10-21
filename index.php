3f77d2590627abe59c2739ce661040f19f592c1e
JonissonGomes <joni_gomes@hotmail.com>
2019-10-21 18:01:48 -0300

    Adicionando a página inicial do web site

---
+++ index.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">	
+
+	<title> Garten's Restaurantes </title>
+
+	<!--Imports das fontes utilizadas-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <!--Import arquivos do materialize.css Offline-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+	
+    <!--Let browser know website is optimized for mobile-->
+     
+</head>
+<body>
+	<!-- Barra de navegação -->
+	<center> O melhor da culinária regional, tradicional e especial bem pertinho de você.</center>
+	<nav class=" amber darken-2" role="navigation">
+    <div class="nav-wrapper container">
+      <a id="logo-container" href="/index.php" class="brand-logo"><img src="img/logo.png" style="margin-top: 5%;"></a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="/cardapio.php">Cardápio</a></li>
+        <li><a href="/cardapio.php">Pratos customizaveis</a></li>
+      </ul>
+		<!-- Lista do Nav no mobile -->
+      <ul id="nav-mobile" class="sidenav">
+		<h4> 
+			<center><img src="/img/logo.png"></h4></center>
+		<hr>
+        <li><a href="cardapio.php">Cardápio</a></li>
+        <li><a href="#">Pratos customizavéis</a></li>
+      </ul><p>
+      <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+    </div>
+  </nav>
+
+<!-- Corpo -->
+<div id="index-banner" class="parallax-container">
+    <div class="section no-pad-bot">
+      <div class="container">
+        <h1 class="header center white-text text-darkthen-4">Garten's Restaurante</h1>
+        <div class="row center">
+          <h5 class="header col s12 white-text">As melhores receitas e os melhores chefes da culinária prontos para satisfazer o seu paladar.</h5>
+        </div>
+      </div>
+    </div>
+    <div class="parallax"><img src="img/comida.png" alt="Unsplashed background img 3"></div>
+  </div>
+
+
+<!--  Informações sobre o estabelecimento -->
+  <div class="container">
+    <div class="section">
+      <div class="row">
+
+	      	<!--  Informativo 1  -->
+	        <div class="col s4 m4 z-depth-1">
+	          <div class="icon-block">
+	            <h2 class="center orange-text"><i class="medium material-icons">ac_unit</i></h2>
+	            <h5 class="center"> Ambientes de qualidade </h5>
+	            <p class="light"> O Garten's não pensa em você como apenas um consumidor, nos preocupamos com o bem estar do nosso cliente. O melhor restaurante da região oferece um estabelecimento climatizado, aconchegante com qualidade para atender todos os clientes. </p>
+	          </div>
+	        </div>
+
+			<!--  Informativo 2  -->
+	        <div class="col s12 m4 ">
+	          <div class="icon-block">
+	            <h2 class="center orange-text"><i class="medium material-icons">access_time</i></h2>
+	            <h5 class="center">Horários</h5>
+
+	            <p class="light"> <center>
+	            	segunda-feira	07:00 ~ 22:00 <br>
+					terça-feira	07:00 ~ 22:00 <br>
+					quarta-feira	07:00 ~ 22:00 <br>
+					quinta-feira	07:00 ~ 22:00 <br>
+					sexta-feira	07:00 ~ 22:00 <br>
+					sábado	Fechado <br>
+					domingo	Fechado </p>
+					</center> 
+	          </div>
+	        </div>
+
+			<!--  Informativo 3  -->
+	        <div class="col s4 m4 z-depth-1">
+	          <div class="icon-block">
+	            <h2 class="center orange-text"><i class="medium material-icons">group</i></h2>
+	            <h5 class="center">Grupo de prossionais</h5>
+	            <p class="light">Nossa cozinha conta com os melhores profissionais do ramo da culinária para atender os mais diferentes gostos e proporcionar aos nossos clientes o melhor da cozinha, utilizando ingredientes de qualidade e suas expertises para apresentar a você uma combinação incrível de sabor. </p>
+	          </div>
+	        </div>
+
+      		</div>
+		  </div>
+		</div>
+		<!-- Fim da primeira parte  -->
+
+		<!-- Começo segunda parte -->
+		<div id="index-banner" class="parallax-container">
+    <div class="section no-pad-bot">
+      <div class="container">
+        <h1 class="header center white-text text-darkthen-4">Visite-nos</h1>
+        <div class="row center">
+          <h5 class="header col s12 white-text">Estamos em nos melhores pontos da região do Recife.</h5>
+        </div>
+      </div>
+    </div>
+    <div class="parallax"><img src="img/outros/interior.jpg" alt="Unsplashed background img 3"></div>
+  </div>
+		<!-- Localidades -->
+<div class="container">
+    <div class="section">
+      <div class="row">
+
+	      	<!-- Prato 1 -->
+	<div class="col s4">
+			<div class="card">
+   				 <div class="card-image waves-effect waves-block waves-light">
+      				<img class="activator" src="img/locais/casaforte.jpg">
+   				 </div>
+    				<div class="card-content">
+    				  <span class="card-title activator orange-text text-darken-3">Casa Forte<h6>(Clique no card para mais informações)</h6> </span>
+    				</div>
+   						 <div class="card-reveal">
+      						<span class="card-title grey-text text-darken-3">Endereço: <i class="material-icons right">close</i></span>
+      						<p>
+      						<i class="material-icons left">room</i>R. da Fundição, 319 <br> Santo Amaro, Recife-PE <i class="material-icons">maps </i>CEP: 50040-100
+    					</div>
+ 					</div>
+ 			</div>
+
+		<!-- Prato 2 -->
+	
+		<div class="col s4">
+			<div class="card">
+   				 <div class="card-image waves-effect waves-block waves-light">
+      				<img class="activator" src="img/locais/santoamaro.jpeg">
+   				 </div>
+    				<div class="card-content">
+    				  <span class="card-title activator orange-text text-darken-3">Santo Amaro<h6>(Clique no card para mais informações)</h6> </span>
+    				</div>
+   						 <div class="card-reveal">
+      						<span class="card-title grey-text text-darken-3">Endereço: <i class="material-icons right">close</i></span>
+      						<p>
+      						<i class="material-icons left">room</i>R. da Fundição, 319 <br> Santo Amaro, Recife-PE <i class="material-icons">maps </i>CEP: 50040-100
+    					</div>
+ 					</div>
+ 			</div>
+ 				<!-- Prato 3 -->
+ 				<div class="col s4">
+			<div class="card">
+   				 <div class="card-image waves-effect waves-block waves-light">
+      				<img class="activator" src="img/locais/recifeantigo.jpg">
+   				 </div>
+    				<div class="card-content">
+    				  <span class="card-title activator orange-text text-darken-3">Recife Antigo<h6>(Clique no card para mais informações)</h6> </span>
+    				</div>
+   						 <div class="card-reveal">
+      						<span class="card-title grey-text text-darken-3">Endereço: <i class="material-icons right">close</i></span>
+      						<p>
+      						<i class="material-icons left">room</i>R. da Fundição, 319 <br> Santo Amaro, Recife-PE <i class="material-icons">maps </i>CEP: 50040-100
+    					</div>
+ 					</div>
+ 			</div>
+		<!-- Fim das localidades -->
+
+		<!-- Footer -->
+
+		<!-- Fim do footer -->
+	<!-- Fim -->
+	<!-- Biblioteca de JavaScript -->
+       <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+      <script src="js/materialize.min.js"></script>
+       <script src="js/init.js"></script>
+
+</body>
+</html>
